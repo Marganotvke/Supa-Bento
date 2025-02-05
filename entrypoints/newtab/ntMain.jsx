0357bda24c0bf5cb99b74a38a832abed0300d197
Marganotvke <58261562+Marganotvke@users.noreply.github.com>
2025-02-05 23:06:57 +0800

    Add async IIFE to top level

--- entrypoints/newtab/ntMain.jsx
+++ entrypoints/newtab/ntMain.jsx
@@ -5,21 +5,23 @@ import { DefaultCONFIG } from '../../assets/defaultConfig.js';
 import imageUrl from '../../assets/background.jpg';
 import { storage } from "wxt/storage";
 
-const usrConfig = storage.defineItem(
-    "sync:usrConfig",
-    {
-        fallback: DefaultCONFIG,
-    }
-);
+(async () => {
+    const usrConfig = storage.defineItem(
+        "sync:usrConfig",
+        {
+            fallback: DefaultCONFIG,
+        }
+    );
 
-const config = await usrConfig.getValue();
-const theme = config.theme;
-const bg = {"backgroundSize": theme.bgImg.bgSize, backgroundImage: [`linear-gradient(${theme.bgImg.bgCol.deg}deg, ${theme.bgImg.bgCol.start}, ${theme.bgImg.bgCol.end})`, `url(${imageUrl})`]};
+    const config = await usrConfig.getValue();
+    const theme = config.theme;
+    const bg = {"backgroundSize": theme.bgImg.bgSize, backgroundImage: [`linear-gradient(${theme.bgImg.bgCol.deg}deg, ${theme.bgImg.bgCol.start}, ${theme.bgImg.bgCol.end})`, `url(${imageUrl})`]};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <div className={`h-full w-full`} style={bg}>
-            <LayoutGenerator config={config}/>
-        </div>
-    </React.StrictMode>,
-);
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <React.StrictMode>
+            <div className={`h-full w-full`} style={bg}>
+                <LayoutGenerator config={config}/>
+            </div>
+        </React.StrictMode>,
+    );
+})();
